b14e5851dbca297246f6a28acd72d6007526e816
Marlo <111030422+Marlowkey@users.noreply.github.com>
2024-09-28 15:04:41 +0800

    add cart functionality

--- resources/views/components/medicine-card.blade.php
+++ resources/views/components/medicine-card.blade.php
@@ -10,6 +10,13 @@
         <h4 id="productPrice">₱{{ number_format($medicine->price, 2) }}</h4>
     </div>
     <div class="cart">
-        <i class="fa-solid fa-cart-shopping"></i>
+        <form action="{{ route('order-items.store') }}" method="POST" class="d-flex flex-column align-items-start mr-4">
+            @csrf
+            <input type="hidden" name="medicine_id" value="{{ $medicine->id }}">
+            <input type="hidden" name="quantity" value="1">
+            <button type="submit" class="btn-sm btn-primary ">
+                <i class="fa-solid fa-cart-shopping me-1"></i>
+            </button>
+        </form>
     </div>
 </div>
