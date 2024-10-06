0bf7cd25e7b348aa2f6e91f71126883c64751f97
Marlo <111030422+Marlowkey@users.noreply.github.com>
2024-10-06 17:26:48 +0800

    fix navbar on profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,16 +27,273 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
+<style>
+    #sidebar-toggle i {
+        font-size: 1.4rem;
+        color: #ffffff;
+    }
+
+    a {
+        transition: background 0.2s, color 0.2s;
+    }
+
+    a:hover,
+    a:focus {
+        text-decoration: none;
+    }
+
+    #wrapper {
+        padding-left: 0;
+        transition: all 0.5s ease;
+        position: relative;
+    }
+
+    #sidebar-wrapper {
+        z-index: 1000;
+        position: fixed;
+        left: 250px;
+        width: 0;
+        height: 100%;
+        margin-left: -250px;
+        overflow-y: auto;
+        overflow-x: hidden;
+        background: #eee;
+        transition: all 0.5s ease;
+    }
+
+    #wrapper.toggled #sidebar-wrapper {
+        width: 250px;
+    }
+
+    .sidebar-nav {
+        position: absolute;
+        top: 35px;
+        width: 250px;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    .sidebar-nav>li {
+        text-indent: 10px;
+        line-height: 42px;
+    }
+
+    .sidebar-nav>li a {
+        display: block;
+        text-decoration: none;
+        color: #1a1a1a;
+        font-weight: 600;
+
+    }
+
+    .sidebar-nav>li>a:hover,
+    .sidebar-nav>li.active>a {
+        text-decoration: none;
+        color: #fff;
+        background: #1551a4;
+    }
+
+    .sidebar-nav>li>a i.fa {
+        width: 60px;
+    }
+
+    #navbar-wrapper {
+        width: 100%;
+        position: absolute;
+        z-index: 2;
+    }
+
+    #wrapper.toggled #navbar-wrapper {
+        position: absolute;
+        margin-right: -250px;
+    }
+
+    #navbar-wrapper .navbar {
+        border-width: 0 0 0 0;
+        background-color: #eee;
+        font-size: 24px;
+        margin-bottom: 0;
+        border-radius: 0;
+    }
+
+    #navbar-wrapper .navbar a {
+        color: #757575;
+    }
+
+    #navbar-wrapper .navbar a:hover {
+        color: #F8BE12;
+    }
+
+    #content-wrapper {
+        width: 100%;
+        position: absolute;
+        padding: 15px;
+        top: 100px;
+    }
+
+    #wrapper.toggled #content-wrapper {
+        position: absolute;
+        margin-right: -250px;
+    }
+
+    @media (min-width: 992px) {
+        #wrapper {
+            padding-left: 250px;
+        }
+
+        #wrapper.toggled {
+            padding-left: 60px;
+        }
+
+        #sidebar-wrapper {
+            width: 250px;
+        }
+
+        #wrapper.toggled #sidebar-wrapper {
+            width: 60px;
+        }
+
+        #wrapper.toggled #navbar-wrapper {
+            position: absolute;
+            margin-right: -190px;
+        }
+
+        #wrapper.toggled #content-wrapper {
+            position: absolute;
+            margin-right: -190px;
+        }
+
+        #navbar-wrapper {
+            position: relative;
+        }
+
+        #wrapper.toggled {
+            padding-left: 60px;
+        }
+
+        #content-wrapper {
+            position: relative;
+            top: 0;
+        }
+
+        #wrapper.toggled #navbar-wrapper,
+        #wrapper.toggled #content-wrapper {
+            position: relative;
+            margin-right: 60px;
+        }
+    }
+
+    @media (min-width: 768px) and (max-width: 991px) {
+        #wrapper {
+            padding-left: 60px;
+        }
+
+        #sidebar-wrapper {
+            width: 60px;
+        }
+
+        #wrapper.toggled #navbar-wrapper {
+            position: absolute;
+            margin-right: -250px;
+        }
+
+        #wrapper.toggled #content-wrapper {
+            position: absolute;
+            margin-right: -250px;
+        }
+
+        #navbar-wrapper {
+            position: relative;
+        }
+
+        #wrapper.toggled {
+            padding-left: 250px;
+        }
+
+        #content-wrapper {
+            position: relative;
+            top: 0;
+        }
+
+        #wrapper.toggled #navbar-wrapper,
+        #wrapper.toggled #content-wrapper {
+            position: relative;
+            margin-right: 250px;
+        }
+    }
+
+    @media (max-width: 767px) {
+        #wrapper {
+            padding-left: 0;
+        }
+
+        #sidebar-wrapper {
+            width: 0;
+        }
+
+        #wrapper.toggled #sidebar-wrapper {
+            width: 250px;
+        }
+
+        #wrapper.toggled #navbar-wrapper {
+            position: absolute;
+            margin-right: -250px;
+        }
+
+        #wrapper.toggled #content-wrapper {
+            position: absolute;
+            margin-right: -250px;
+        }
+
+        #navbar-wrapper {
+            position: relative;
+        }
+
+        #wrapper.toggled {
+            padding-left: 250px;
+        }
+
+        #content-wrapper {
+            position: relative;
+            top: 0;
+        }
+
+        #wrapper.toggled #navbar-wrapper,
+        #wrapper.toggled #content-wrapper {
+            position: relative;
+            margin-right: 250px;
+        }
+    }
+</style>
 
 <body>
-    @if (request()->is('profile'))
-        @include('layouts.partials.profile-nav')
-    @else
+    @if (auth()->check() && auth()->user()->role === 'customer')
         @include('layouts.partials.navbar')
+        <div id="app">
+            @yield('content')
+        </div>
+    @else
+        @include('layouts.partials.dashboard-nav')
+        <div id="app" class="d-flex">
+            <nav class="flex-shrink-0">
+                @include('layouts.partials.admin-sidebar')
+            </nav>
+            <div class="flex-grow-1 p-3">
+                @yield('content')
+            </div>
+        </div>
+
+        <script>
+            const $button = document.querySelector('#sidebar-toggle');
+            const $wrapper = document.querySelector('#wrapper');
+
+            $button.addEventListener('click', (e) => {
+                e.preventDefault();
+                $wrapper.classList.toggle('toggled');
+            });
+        </script>
     @endif
-    <div id="app">
-        @yield('content')
-    </div>
     @include('layouts.partials.footer')
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.guest')
 @section('content')
     <x-main-content />
 @endsection
