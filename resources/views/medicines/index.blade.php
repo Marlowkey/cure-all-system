01855adaf75d34cd7091273a7737f2a2a5d57066
nel-johnsen <njdlalano@catsu.edu.ph>
2024-10-29 18:41:13 +0800

    modified:   resources/views/medicines/index.blade.php

--- resources/views/medicines/index.blade.php
+++ resources/views/medicines/index.blade.php
@@ -13,14 +13,21 @@
 </section>
 
 <section id="product" class="section-p1">
+      <!-- Sticky Prescription Notice (Large Screens Only) -->
+    <div class="alert alert-warning text-center sticky-top fs-3 d-none d-lg-block" role="alert" style="top: 115px;">
+        <strong>Notice:</strong> All RX medications require a valid prescription to be uploaded.
+    </div>
+    <!-- Product Availability Section -->
     <p>Available</p>
     <div class="pro-container">
-
         @foreach ($medicines as $medicine)
             <x-medicine-card :medicine="$medicine" />
         @endforeach
     </div>
 </section>
+
+
+
 
 
 <nav class="d-flex justify-content-center">
